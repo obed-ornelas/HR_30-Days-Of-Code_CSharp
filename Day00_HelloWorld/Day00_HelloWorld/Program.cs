0df54f4c8bab5891d1cbae274670902b1ea59b08
Obed <obed.j.ornelas@gmail.com>
2020-06-03 20:09:21 -0400

    Day 0: Finished Challenge (Hello, World)

--- Day00_HelloWorld/Day00_HelloWorld/Program.cs
+++ Day00_HelloWorld/Day00_HelloWorld/Program.cs
@@ -3,6 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*
+  OO 06-03-2020
+
+  Task
+  ----------------------------------------------------------------------------------------------------------
+  To complete this challenge, you must save a line of input from stdin to a variable, print 'Hello, World.'
+  on a single line, and finally print the value of your variable on a second line.
+
+  Input Format
+  ----------------------------------------------------------------------------------------------------------
+  A single line of text denoting 'inputString' (the variable whose contents must be printed).
+
+  Sample Input
+  ----------------------------------------------------------------------------------------------------------
+  Welcome to 30 Days of Code!
+
+  Sample Output
+  ----------------------------------------------------------------------------------------------------------
+  Hello, World.
+  Welcome to 30 Days of Code!
+
+*/
 
 namespace Day00_HelloWorld
 {
