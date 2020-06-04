3f9adc1bc81baf15bdf1bf0e0826161dcf4865f8
Obed <obed.j.ornelas@gmail.com>
2020-06-04 18:37:42 -0400

    Day 3: Finished Challenge (Intro to Conditional Statements)

--- Day03_IntroConditionalStatements/Day03_IntroConditionalStatements/Program.cs
+++ Day03_IntroConditionalStatements/Day03_IntroConditionalStatements/Program.cs
@@ -4,6 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+  OO 06-04-2020
+
+  Task
+  ----------------------------------------------------------------------------------------------------------
+  Given an integer, , perform the following conditional actions:
+  If 'n' is odd, print Weird
+  If 'n' is even and in the inclusive range of 2 to 5, print Not Weird
+  If 'n' is even and in the inclusive range of 6 to 20, print Weird
+  If 'n' is even and greater than 20, print Not Weird
+  Complete the stub code provided in your editor to print whether or not  is weird.
+
+  Input Format
+  ----------------------------------------------------------------------------------------------------------
+  A single line containing a positive integer, 'n'.
+
+  Sample Input
+  ----------------------------------------------------------------------------------------------------------
+  3
+
+  Sample Output
+  ----------------------------------------------------------------------------------------------------------
+  Weird
+
+*/
+
 namespace Day03_IntroConditionalStatements
 {
     class Program
