df3442bb0100c76057d1344d6f6a1f286041b1b9
Obed <obed.j.ornelas@gmail.com>
2020-06-03 20:49:14 -0400

    Day 1: Finished Challenge (Data Types)

--- Day01_DataTypes/Day01_DataTypes/Program.cs
+++ Day01_DataTypes/Day01_DataTypes/Program.cs
@@ -4,6 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+  OO 06-03-2020
+
+  Task
+  ----------------------------------------------------------------------------------------------------------
+  Complete the code in the editor below. The variables 'i', 'd', and 's' are already declared and
+  initialized for you. You must:
+
+    1. Declare 3 variables: one of type int, one of type double, and one of type String.
+    2. Read 3 lines of input from stdin (according to the sequence given in the Input Format section below)
+       and initialize your 3 variables.
+    3. Use the + operator to perform the following operations:
+        1. Print the sum of 'i' plus your int variable on a new line.
+        2. Print the sum of 'd' plus your double variable to a scale of one decimal place on a new line.
+        3. Concatenate 's' with the string you read as input and print the result on a new line.
+
+  Input Format
+  ----------------------------------------------------------------------------------------------------------
+  The first line contains an integer that you must sum with 'i'.
+  The second line contains a double that you must sum with 'd'.
+  The third line contains a string that you must concatenate with 's'.
+
+  Sample Input
+  ----------------------------------------------------------------------------------------------------------
+  12
+  4.0
+  is the best place to learn and practice coding!
+
+  Sample Output
+  ----------------------------------------------------------------------------------------------------------
+  16
+  8.0
+  HackerRank is the best place to learn and practice coding!
+
+*/
+
 namespace Day01_DataTypes
 {
     class Program
