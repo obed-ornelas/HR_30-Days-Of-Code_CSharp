68ae1570c9bd1b39a44daed45239f12d04628e16
Obed <obed.j.ornelas@gmail.com>
2020-06-03 20:01:40 -0400

    Day 0: Add logic and personal changes

--- Day00_HelloWorld/Day00_HelloWorld/Program.cs
+++ Day00_HelloWorld/Day00_HelloWorld/Program.cs
@@ -10,16 +10,11 @@ namespace Day00_HelloWorld
     {
         static void Main(string[] args)
         {
-            // Declare a variable named 'inputString' to hold our input.
             String inputString;
-
-            // Read a full line of input from stdin (cin) and save it to our variable, input_string.
             inputString = Console.ReadLine();
 
-            // Print a string literal saying "Hello, World." to stdout using cout.
             Console.WriteLine("Hello, World.");
-
-            // TODO: Write a line of code here that prints the contents of input_string to stdout.
+            Console.WriteLine(inputString);
         }
     }
 }
