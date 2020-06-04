046e9330055e25a224e9c16964bdcee421c3561d
Obed <obed.j.ornelas@gmail.com>
2020-06-04 18:20:05 -0400

    Day 3: Given code

---
+++ Day03_IntroConditionalStatements/Day03_IntroConditionalStatements/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day03_IntroConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
