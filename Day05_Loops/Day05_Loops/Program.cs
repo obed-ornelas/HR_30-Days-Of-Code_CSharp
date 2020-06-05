3e5a08e4988914287b465d283ca1bed3dc6f04b5
Obed <obed.j.ornelas@gmail.com>
2020-06-04 22:31:54 -0400

    Day 5: Added logic and personal changes

--- Day05_Loops/Day05_Loops/Program.cs
+++ Day05_Loops/Day05_Loops/Program.cs
@@ -8,9 +8,21 @@ namespace Day05_Loops
 {
     class Program
     {
+        static void printFirst10Multiples(int number)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", number, i, number * i);
+            }
+        }
+
         static void Main(string[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
+
+            printFirst10Multiples(n);
+
+            Console.ReadLine();
         }
     }
 }
