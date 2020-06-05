d852c76a0c013493c28aa2346df95fa212ecbc5d
Obed <obed.j.ornelas@gmail.com>
2020-06-04 22:47:31 -0400

    Day 6: Given code

---
+++ Day06_Review/Day06_Review/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day06_Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        }
+    }
+}
