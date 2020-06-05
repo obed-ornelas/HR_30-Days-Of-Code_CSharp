47fdc43aeb7206b42c2b1bfcdd8fb5ea0c810182
Obed <obed.j.ornelas@gmail.com>
2020-06-05 00:04:59 -0400

    Day 7: Given code

---
+++ Day07_Arrays/Day07_Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        }
+    }
+}
