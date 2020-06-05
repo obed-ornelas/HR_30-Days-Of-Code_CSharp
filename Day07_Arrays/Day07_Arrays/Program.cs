b54a1d8589b9a953b0febbbb79cab9c82a9fd5df
Obed <obed.j.ornelas@gmail.com>
2020-06-05 00:06:52 -0400

    Day 7: Added logic and personal changes

--- Day07_Arrays/Day07_Arrays/Program.cs
+++ Day07_Arrays/Day07_Arrays/Program.cs
@@ -1,18 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Day07_Arrays
 {
     class Program
     {
+        static int[] Reverse(int[] array)
+        {
+            int size = array.Length;
+            int[] reversed = new int[size];
+
+            for (int i = size - 1, j = 0; i >= 0; i--, j++)
+            {
+                reversed[j] = array[i];
+            }
+
+            return reversed;
+        }
+
         static void Main(string[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
 
-            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), x => Convert.ToInt32(x));
+
+            Console.WriteLine(string.Join(" ", Reverse(array)));
+
+            Console.ReadLine();
         }
     }
 }
