9e8076b03dda0d4aa205a20dcfd1148e5b4a10c8
Obed <obed.j.ornelas@gmail.com>
2020-06-07 02:16:28 -0400

    Day 9: Added logic and personal changes

--- Day09_Recursion/Day09_Recursion/Program.cs
+++ Day09_Recursion/Day09_Recursion/Program.cs
@@ -1,28 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.IO;
 
 namespace Day09_Recursion
 {
     class Program
     {
-        // Complete the factorial function below.
-        static int factorial(int n)
+        static int Factorial(int n)
         {
-
-
+            if (n <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
         }
 
         static void Main(string[] args)
         {
-
-            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+            TextWriter textWriter = new StreamWriter("OUTPUT.txt", true);
 
             int n = Convert.ToInt32(Console.ReadLine());
 
-            int result = factorial(n);
+            int result = Factorial(n);
 
             textWriter.WriteLine(result);
 
