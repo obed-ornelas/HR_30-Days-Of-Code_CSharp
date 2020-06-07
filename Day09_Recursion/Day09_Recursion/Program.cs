aff25269539908ee76e515642bb83c2adb0acdbe
Obed <obed.j.ornelas@gmail.com>
2020-06-07 01:51:34 -0400

    Day 9: Given code

---
+++ Day09_Recursion/Day09_Recursion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day09_Recursion
+{
+    class Program
+    {
+        // Complete the factorial function below.
+        static int factorial(int n)
+        {
+
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = factorial(n);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
