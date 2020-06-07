935b23eee944cfd5d16d05dc2cc00b98605ccbf7
Obed <obed.j.ornelas@gmail.com>
2020-06-07 01:22:54 -0400

    Day 9: Added logic and personal changes

--- Day08_DictionariesMaps/Day08_DictionariesMaps/Program.cs
+++ Day08_DictionariesMaps/Day08_DictionariesMaps/Program.cs
@@ -7,7 +7,31 @@ namespace Day08_DictionariesMaps
     {
         static void Main(string[] args)
         {
-            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+
+            int entries = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < entries; i++)
+            {
+                string[] entry = Console.ReadLine().Split(' ');
+                phoneBook.Add(entry[0], entry[1]);
+            }
+
+            string name = Console.ReadLine();
+
+            while (!string.IsNullOrEmpty(name))
+            {
+                if (phoneBook.TryGetValue(name, out string phoneNumber))
+                {
+                    Console.WriteLine("{0}={1}", name, phoneNumber);
+                }
+                else
+                {
+                    Console.WriteLine("Not found");
+                }
+
+                name = Console.ReadLine();
+            }
         }
     }
 }
