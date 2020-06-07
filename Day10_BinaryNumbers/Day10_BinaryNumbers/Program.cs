f18d9bd5ccad828f778e6011dc9fa6e1d10444b2
Obed <obed.j.ornelas@gmail.com>
2020-06-07 16:39:15 -0400

    Day 10: Added logic and personal changes

--- Day10_BinaryNumbers/Day10_BinaryNumbers/Program.cs
+++ Day10_BinaryNumbers/Day10_BinaryNumbers/Program.cs
@@ -1,16 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace Day10_BinaryNumbers
 {
     class Program
     {
+        static int CountConsecutiveNumber(int number, string binaryNumber)
+        {
+            int max = 0, currentMax = 0;
+
+            for (int i = 0; i < binaryNumber.Length; i++)
+            {
+                if (Int32.Parse(binaryNumber[i].ToString()) == number)
+                {
+                    currentMax++;
+
+                    if (currentMax > max)
+                    {
+                        max = currentMax;
+                    }
+                }
+                else
+                {
+                    currentMax = 0;
+                }
+            }
+
+            return max;
+        }
+
+        static string ToBinary(int number)
+        {
+            int dividend = number, remainder;
+            string binary = "";
+
+            while (dividend > 0)
+            {
+                remainder = dividend % 2;
+                binary = remainder.ToString() + binary;
+                dividend = dividend / 2;
+            }
+
+            return binary;
+        }
+
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(CountConsecutiveNumber(1, ToBinary(number)));
+
+            Console.ReadLine();
         }
     }
 }
