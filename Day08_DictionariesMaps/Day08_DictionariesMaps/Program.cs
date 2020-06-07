cf918774f9a2d9b0550a4b49922d57054f167df3
Obed <obed.j.ornelas@gmail.com>
2020-06-07 01:03:55 -0400

    Day 8: Given code

---
+++ Day08_DictionariesMaps/Day08_DictionariesMaps/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day08_DictionariesMaps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        }
+    }
+}
