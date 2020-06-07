a2b8c6cda5a34c5bd948f39be673a4e4bb1c6212
Obed <obed.j.ornelas@gmail.com>
2020-06-07 02:18:42 -0400

    Day 9: Finished Challenge (Recursion)

--- Day09_Recursion/Day09_Recursion/Program.cs
+++ Day09_Recursion/Day09_Recursion/Program.cs
@@ -1,6 +1,28 @@
 using System;
 using System.IO;
 
+/*
+  OO 06-07-2020
+
+  Task
+  ----------------------------------------------------------------------------------------------------------
+  Write a factorial function that takes a positive integer, 'N' as a parameter and prints the result of N!
+  (N factorial).
+
+  Input Format
+  ----------------------------------------------------------------------------------------------------------
+  A single integer, N (the argument to pass to factorial).
+
+  Sample Input
+  ----------------------------------------------------------------------------------------------------------
+  3
+
+  Sample Output
+  ----------------------------------------------------------------------------------------------------------
+  6
+
+*/
+
 namespace Day09_Recursion
 {
     class Program
