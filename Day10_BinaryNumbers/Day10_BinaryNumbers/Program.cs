f22e1c09505ec96e6ee3a7ded11482940b0700b7
Obed <obed.j.ornelas@gmail.com>
2020-06-07 15:25:21 -0400

    Day 10: Given code

---
+++ Day10_BinaryNumbers/Day10_BinaryNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10_BinaryNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
