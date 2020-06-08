b7db57745c6eb4dd04c2b1f7727cffe35b5e407c
Obed <obed.j.ornelas@gmail.com>
2020-06-07 23:18:04 -0400

    Day 11: Given code

---
+++ Day11_2DArrays/Day11_2DArrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11_2DArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] arr = new int[6][];
+
+            for (int i = 0; i < 6; i++)
+            {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+        }
+    }
+}
