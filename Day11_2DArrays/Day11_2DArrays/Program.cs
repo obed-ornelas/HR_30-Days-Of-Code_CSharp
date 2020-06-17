ae70e0581abeceeda3f4ed49abfc2504d0b6435d
Obed <obed.j.ornelas@gmail.com>
2020-06-17 00:15:38 -0400

    Day 11: Added logic and personal changes

--- Day11_2DArrays/Day11_2DArrays/Program.cs
+++ Day11_2DArrays/Day11_2DArrays/Program.cs
@@ -1,13 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Day11_2DArrays
 {
     class Program
     {
+        static int GetHourGlassSum(int[][] map, int x, int y)
+        {
+            int sum = map[x - 1][y - 1] +
+                      map[x - 1][y] +
+                      map[x - 1][y + 1] +
+                      map[x][y] +
+                      map[x + 1][y - 1] +
+                      map[x + 1][y] +
+                      map[x + 1][y + 1];
+
+            return sum;
+        }
+
+        static int FindMaxHourGlassSum(int[][] map)
+        {
+            int maxSum = 0;
+
+            for (int row = 1; row < map.Length - 1; row++)
+            {
+                for (int col = 1; col < map.Length - 1; col++)
+                {
+                    int sum = GetHourGlassSum(map, row, col);
+
+                    if (row == 1 && col == 1 || sum > maxSum)
+                    {
+                        maxSum = sum;
+                    }
+                }
+            }
+
+            return maxSum;
+        }
+
         static void Main(string[] args)
         {
             int[][] arr = new int[6][];
@@ -16,6 +45,9 @@ namespace Day11_2DArrays
             {
                 arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
             }
+
+            Console.WriteLine(FindMaxHourGlassSum(arr));
+            Console.ReadLine();
         }
     }
 }
