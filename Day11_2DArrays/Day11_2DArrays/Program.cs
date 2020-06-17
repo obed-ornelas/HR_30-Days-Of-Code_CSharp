5e2ae21ee1edae6f850ed68dda90d26fb99c8481
Obed <obed.j.ornelas@gmail.com>
2020-06-17 00:18:35 -0400

    Day 11: Finished Challenge (2D Arrays)

--- Day11_2DArrays/Day11_2DArrays/Program.cs
+++ Day11_2DArrays/Day11_2DArrays/Program.cs
@@ -1,4 +1,36 @@
-﻿using System;
+using System;
+
+/*
+  OO 06-17-2020
+
+  Context
+  ----------------------------------------------------------------------------------------------------------
+  Given a 6x6 2D Array, A
+
+  1 1 1 0 0 0
+  0 1 0 0 0 0
+  1 1 1 0 0 0
+  0 0 0 0 0 0
+  0 0 0 0 0 0
+  0 0 0 0 0 0
+
+  We define an hourglass in A to be a subset of values with indices falling in this pattern in A's graphical representation:
+
+  a b c
+    d
+  e f g
+
+  There are 16 hourglasses in A, and an hourglass sum is the sum of an hourglass' values.
+
+  Task
+  ----------------------------------------------------------------------------------------------------------
+  Calculate the hourglass sum for every hourglass in A, then print the maximum hourglass sum.
+
+  Input Format
+  ----------------------------------------------------------------------------------------------------------
+  There are 6 lines of input, where each line contains 6 space-separated integers describing 2D Array A;
+  every value in A will be in the inclusive range of -9 to 9.
+*/
 
 namespace Day11_2DArrays
 {
