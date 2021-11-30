19a4490d304228ee7ed0ebff0c5fedd21b8b87e7
obed-ornelas <43757211+obed-ornelas@users.noreply.github.com>
2021-11-29 21:59:39 -0500

    Feature/day13 abstract classes (#1)
    * Add initial Visual Studio solution and given code
    
    * Format solution with StyleCop
    
    * Convert protected fields to public properties in Book and MyBook classes
    
    * Add project property to generate documentation file on output
    
    * Add outsideNamespace StyleCop config since only one namespace exists within solution
    
    * Renamed solution/project to the appropriate assignment

---
+++ Day13_AbstractClasses/Day13_AbstractClasses/Book.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="Book.cs" company="OO">
+// Copyright (c) OO. All rights reserved.
+// </copyright>
+
+namespace Day13_AbstractClasses
+{
+    /// <summary>
+    /// Class <c>Book</c> models a book.
+    /// The following is the original class given by the HackerRank Challenge.
+    /// </summary>
+    public abstract class Book
+    {
+        /// <summary>
+        /// Title of book.
+        /// </summary>
+        private string title;
+
+        /// <summary>
+        /// Author of book.
+        /// </summary>
+        private string author;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Book"/> class.
+        /// </summary>
+        /// <param name="title">Title of Book.</param>
+        /// <param name="author">Author of Book.</param>
+        public Book(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+
+        /// <summary>
+        /// Gets or Sets the Title of the Book.
+        /// </summary>
+        public string Title
+        {
+            get => this.title;
+            set => this.title = value;
+        }
+
+        /// <summary>
+        /// Gets or Sets the Author of the Book.
+        /// </summary>
+        public string Author
+        {
+            get => this.author;
+            set => this.author = value;
+        }
+
+        /// <summary>
+        /// Prints all of the Book's properties.
+        /// </summary>
+        public abstract void Display();
+    }
+}
---
+++ Day13_AbstractClasses/Day13_AbstractClasses/MyBook.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="MyBook.cs" company="OO">
+// Copyright (c) OO. All rights reserved.
+// </copyright>
+using System;
+
+namespace Day13_AbstractClasses
+{
+    /// <summary>
+    /// Class that subclasses the abstract <see cref="Book"/>
+    /// which has a Price attribute.
+    /// </summary>
+    public class MyBook : Book
+    {
+        private int price;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyBook"/> class.
+        /// </summary>
+        /// <param name="title">Title of <see cref="MyBook"/>.</param>
+        /// <param name="author">Author of <see cref="MyBook"/>.</param>
+        /// <param name="price">Price of <see cref="MyBook"/>.</param>
+        public MyBook(string title, string author, int price)
+            : base(title, author)
+        {
+            this.price = price;
+        }
+
+        /// <summary>
+        /// Gets or Sets the Price of the Book.
+        /// </summary>
+        public int Price
+        {
+            get => this.price;
+            set => this.price = value;
+        }
+
+        /// <inheritdoc/>
+        public override void Display()
+        {
+            Console.WriteLine("Title: {0}", this.Title);
+            Console.WriteLine("Author: {0}", this.Author);
+            Console.WriteLine("Price: {0}", this.Price);
+        }
+    }
+}
