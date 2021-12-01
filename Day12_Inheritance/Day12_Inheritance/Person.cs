8a92280b1004799ac8ca25028c540c633d2180a5
Obed Ornelas <obed.j.ornelas@gmail.com>
2021-11-30 22:27:50 -0500

    Add initial VS Solution, given code, and README

---
+++ Day12_Inheritance/Day12_Inheritance/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day12_Inheritance
+{
+    class Person
+    {
+		protected string firstName;
+		protected string lastName;
+		protected int id;
+
+		public Person() { }
+		public Person(string firstName, string lastName, int identification)
+		{
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.id = identification;
+		}
+		public void printPerson()
+		{
+			Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id);
+		}
+	}
+}
---
+++ Day12_Inheritance/Day12_Inheritance/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day12_Inheritance
+{
+    class Student : Person
+    {
+        private int[] testScores;
+
+        /*	
+        *   Class Constructor
+        *   
+        *   Parameters: 
+        *   firstName - A string denoting the Person's first name.
+        *   lastName - A string denoting the Person's last name.
+        *   id - An integer denoting the Person's ID number.
+        *   scores - An array of integers denoting the Person's test scores.
+        */
+        // Write your constructor here
+
+        /*	
+        *   Method Name: Calculate
+        *   Return: A character denoting the grade.
+        */
+        // Write your method here
+    }
+}
