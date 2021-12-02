4937bf1080d2eec980d7a6836bea402d9632b672
Obed Ornelas <obed.j.ornelas@gmail.com>
2021-12-02 01:21:04 -0500

    Add initial solution

--- Day12_Inheritance/Day12_Inheritance/Student.cs
+++ Day12_Inheritance/Day12_Inheritance/Student.cs
@@ -19,10 +19,60 @@ namespace Day12_Inheritance
         */
         // Write your constructor here
 
+        public Student(string firstName, string lastName, int idNumber, int[] scores) : base(firstName, lastName, idNumber)
+        {
+            testScores = scores;
+        }
+
         /*	
         *   Method Name: Calculate
         *   Return: A character denoting the grade.
         */
         // Write your method here
+
+        private int GetScoresAverage()
+        {
+            int average = 0, total = 0;
+
+            foreach (int score in testScores)
+            {
+                total += score;
+            }
+
+            average = total / testScores.Length;
+
+            return average;
+        }
+
+
+        public char Calculate()
+        {
+            var average = GetScoresAverage();
+
+            if (average >= 90 && average <= 100)
+            {
+                return 'O';
+            }
+            else if (average >= 80 && average < 90)
+            {
+                return 'E';
+            }
+            else if (average >= 70 && average < 80)
+            {
+                return 'A';
+            }
+            else if (average >= 55 && average < 70)
+            {
+                return 'P';
+            }
+            else if (average >= 40 && average < 55)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'T';
+            }
+        }
     }
 }
