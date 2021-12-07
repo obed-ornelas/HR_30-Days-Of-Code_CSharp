43fc37ef9c35d98ae85a331341b5d3360f1ab7da
Obed Ornelas <obed.j.ornelas@gmail.com>
2021-12-07 00:44:50 -0500

    Add solution for task

--- Day12_Inheritance/Day12_Inheritance/Person.cs
+++ Day12_Inheritance/Day12_Inheritance/Person.cs
@@ -2,22 +2,22 @@
 
 namespace Day12_Inheritance
 {
-    class Person
+    public class Person
     {
-		protected string firstName;
-		protected string lastName;
-		protected int id;
+        protected string firstName;
+        protected string lastName;
+        protected int id;
 
-		public Person() { }
-		public Person(string firstName, string lastName, int identification)
-		{
-			this.firstName = firstName;
-			this.lastName = lastName;
-			this.id = identification;
-		}
-		public void printPerson()
-		{
-			Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id);
-		}
-	}
+        public Person(string firstName, string lastName, int id)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.id = id;
+        }
+
+        public void PrintPerson()
+        {
+            Console.WriteLine("Name: " + this.lastName + ", " + this.firstName + "\nID: " + this.id);
+        }
+    }
 }
--- Day12_Inheritance/Day12_Inheritance/Student.cs
+++ Day12_Inheritance/Day12_Inheritance/Student.cs
@@ -1,53 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Day12_Inheritance
+﻿namespace Day12_Inheritance
 {
-    class Student : Person
+    public class Student : Person
     {
-        private int[] testScores;
+        private readonly int[] testScores;
 
-        /*	
-        *   Class Constructor
-        *   
-        *   Parameters: 
-        *   firstName - A string denoting the Person's first name.
-        *   lastName - A string denoting the Person's last name.
-        *   id - An integer denoting the Person's ID number.
-        *   scores - An array of integers denoting the Person's test scores.
-        */
-        // Write your constructor here
-
-        public Student(string firstName, string lastName, int idNumber, int[] scores) : base(firstName, lastName, idNumber)
+        public Student(string firstName, string lastName, int idNumber, int[] scores)
+            : base(firstName, lastName, idNumber)
         {
-            testScores = scores;
+            this.testScores = scores;
         }
-
-        /*	
-        *   Method Name: Calculate
-        *   Return: A character denoting the grade.
-        */
-        // Write your method here
-
-        private int GetScoresAverage()
-        {
-            int average = 0, total = 0;
-
-            foreach (int score in testScores)
-            {
-                total += score;
-            }
-
-            average = total / testScores.Length;
-
-            return average;
-        }
-
 
         public char Calculate()
         {
-            var average = GetScoresAverage();
+            var average = this.GetScoresAverage();
 
             if (average >= 90 && average <= 100)
             {
@@ -73,6 +38,20 @@ namespace Day12_Inheritance
             {
                 return 'T';
             }
+        }
+
+        private int GetScoresAverage()
+        {
+            int total = 0;
+
+            foreach (int score in this.testScores)
+            {
+                total += score;
+            }
+
+            int average = total / this.testScores.Length;
+
+            return average;
         }
     }
 }
